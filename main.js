b28617cc9a931cbfbd628b854622ff8c615b0858
Sergio Torres <sergio.torres@idr.edu.mx>
2024-03-15 14:28:48 -0600

    api fetch

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ import './styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+import { getUsersUsignFetch } from './src/js/getUsers'
+
+const getUsers = async()=>{
+    const users = await getUsersUsignFetch("https://reqres.in/api/users?page=2");
+    console.log( users );
+}
+
+getUsers();
---
+++ src/js/getUsers.js
@@ -0,0 +1,18 @@
+
+
+
+const getUsersUsignFetch = async( url ) => {
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const users = data.data;
+        return users;
+        
+    } catch (error) {
+        console.error( error );
+    }
+
+}
+
+export {getUsersUsignFetch};
